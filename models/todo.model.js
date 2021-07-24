b9939ba39648a5033e2e917c1136435d7fdfa227
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-07-24 11:56:33 +0600

    Schema added with proper database validation

--- models/todo.model.js
+++ models/todo.model.js
@@ -1,11 +1,29 @@
+const mongoose   = require("mongoose");
+const { Schema } = mongoose;
+
+const todoSchema = new Schema({
+    title: {
+        type    : String,
+        required: [true, 'Please give todo title !']
+    },
+    status: {
+        type    : String,
+        enum    : { values: ['Pending', 'Done'], message: 'Todo Status is not correct !' },
+        default : 'Pending',
+        required: [true, 'Please give todo status']
+    },
+    description: {
+        type    : String,
+        required: false,
+        min     : [5, 'Please give minimum 5 characters for todo description !']
+    }
+});
+
 module.exports = mongoose => {
     const Todo = mongoose.model(
         "Todo",
         mongoose.Schema(
-            {
-                title : String,
-                status: String
-            },
+            todoSchema,
             { timestamps: true }
         )
     );
